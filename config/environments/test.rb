82213bb62b2b68e20f399130af6ab0eeaa00153c
Saulo Arruda <sauloarruda@gmail.com>
2010-02-24 13:21:08 -0400

    ajustes no ambiente para execução dos testes (autotest e rcov)

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,4 +27,8 @@ config.action_mailer.delivery_method = :test
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
 
-  config.gem 'rspec-rails', :version => '>= 1.3.2', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
+config.gem 'rspec-rails', :version => '>= 1.3.2', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
+
+# carlos brando autotest feature (http://www.claytonlz.com/index.php/2009/04/how-to-setup-rspec-cucumber-webrat-rcov-and-autotest-on-leopard/)
+ENV['AUTOFEATURE'] = "true"
+ENV['RSPEC'] = "true"
