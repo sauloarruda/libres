a4f93929968c71e831185fc5ebc7b3702fb6e0db
Saulo Arruda <sauloarruda@gmail.com>
2010-02-26 11:49:42 -0400

    clean the master branch, moved to libres-rails

--- app/models/tag.rb
+++
@@ -1,9 +0,0 @@
-class Tag < ActiveRecord::Base
-  
-  validates_presence_of :name
-  
-  def Tag.for_lookup
-    Tag.find(:all, { :select => :name, :order => :name }).collect { |tag| tag.name }
-  end
-  
-end
--- db/migrate/20100222190633_alter_author_add_name.rb
+++
@@ -1,9 +0,0 @@
-class AlterAuthorAddName < ActiveRecord::Migration
-  def self.up
-    add_column :authors, :name, :string
-  end
-
-  def self.down
-    remove_column :authors, :name
-  end
-end
